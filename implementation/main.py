a2f795d2363931c4b54101fd735df952ed21ea3a
caiofov <cfoviana@alu.ufc.br>
2022-10-25 08:39:50 -0300

    main

---
+++ implementation/main.py
@@ -0,0 +1,11 @@
+from emulator import MemoryEmulator
+
+mem = MemoryEmulator()
+mem.write_word(3, 5)
+print(mem.read_word(3))
+
+mem.write_byte(80, 200)
+print(mem.read_byte(80))
+
+mem.write_word(4, 0xAAFFEEC1)
+print(mem.read_byte(18))
