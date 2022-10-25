e5564434f7737e835572fbf35917b19b0df9b986
caiofov <cfoviana@alu.ufc.br>
2022-10-25 08:44:37 -0300

    rename variables

--- implementation/emulator.py
+++ implementation/emulator.py
@@ -7,22 +7,22 @@ class MemoryEmulator:
         # 1 word = 32 bits (4 bytes)
 
     @staticmethod
-    def _get_pos(end: int) -> int:
-        return end & 0b111111111111111111
+    def _get_pos(pos: int) -> int:
+        return pos & 0b111111111111111111
 
-    def read_word(self, end: int) -> int:
+    def read_word(self, memory_address: int) -> int:
         # reads 32 bits
-        pos = self._get_pos(end)
+        pos = self._get_pos(memory_address)
         return self._memory[pos]
 
-    def write_word(self, end: int, val: int) -> None:
+    def write_word(self, memory_address: int, value: int) -> None:
         # writes 32 bits
-        pos = self._get_pos(end)
-        val = val & 0xFFFFFFFF
-        self._memory[pos] = val
+        pos = self._get_pos(memory_address)
+        value = value & 0xFFFFFFFF
+        self._memory[pos] = value
 
-    def read_byte(self, end: int) -> int:
-        pos = self._get_pos(end)
+    def read_byte(self, memory_address: int) -> int:
+        pos = self._get_pos(memory_address)
         end_word = pos >> 2
         val_word = self._memory[end_word]
 
@@ -31,14 +31,14 @@ class MemoryEmulator:
         val_byte = val_byte & 0xFF
         return val_byte
 
-    def write_byte(self, end: int, val: int) -> None:
-        pos = self._get_pos(end)
-        val = val & 0xFF
+    def write_byte(self, memory_address: int, value: int) -> None:
+        pos = self._get_pos(memory_address)
+        value = value & 0xFF
         end_word = pos >> 2
         val_word = self._memory[end_word]
         end_byte = pos & 0b11
         mask = ~(0xFF << (end_byte << 3))
         val_word = val_word & mask
-        val = val << (end_byte << 3)
-        val_word = val_word | val
+        value = value << (end_byte << 3)
+        val_word = val_word | value
         self._memory[end_word] = val_word
