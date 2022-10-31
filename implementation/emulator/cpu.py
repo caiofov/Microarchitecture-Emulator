b5c17206b33a7fe9820715b0204f2a323868990e
caiofov <cfoviana@alu.ufc.br>
2022-10-30 22:37:00 -0300

    refactor control code

--- implementation/emulator/cpu.py
+++ implementation/emulator/cpu.py
@@ -28,20 +28,9 @@ class CPU:
                 self._memory.write_byte(byte_address, int.from_bytes(byte, "little"))
                 byte_address += 1
 
-    def add_instruction(self, instruction: int, index: Optional[int] = None) -> None:
-        """Adds a new instruction to the firmware
-        Args:
-            instruction (int): Instruction to be added
-            index (Optional[int], optional): New struction's index(None to append). Defaults to None.
-        """
-        if not (idx := index):
-            idx = self._last_inst_idx
-            self._last_inst_idx += 1
-        self.firmware[idx] = instruction
-
     def halt(self) -> None:
         """Halt instruction"""
-        self.add_instruction(0b00000000000000000000000000000000, 255)
+        self.firmware[255] = 0b00000000000000000000000000000000
 
     def _read_registers(self, register_number: int) -> None:
         self._bus.BUS_A = self._regs.H
@@ -127,54 +116,66 @@ class CPU:
 
         return str(output)
 
+    def _main_op(self) -> None:
+        # main: PC <- PC + 1; MBR <- read_byte(PC); GOTO MBR
+        self.firmware[0] = 0b000000000_100_00_110101_001000_001_001
+
+    def _add_op(self) -> None:
+        # X = X + mem[address]
+        ##2: PC <- PC + 1; MBR <- read_byte(PC); GOTO 3
+        self.firmware[2] = 0b000000011_000_00_110101_001000_001_001
+        ##3: MAR <- MBR; read_word; GOTO 4
+        self.firmware[3] = 0b000000100_000_00_010100_100000_010_010
+        ##4: H <- MDR; GOTO 5
+        self.firmware[4] = 0b000000101_000_00_010100_000001_000_000
+        ##5: X <- H + X; GOTO 0
+        self.firmware[5] = 0b000000000_000_00_111100_000100_000_011
+
+    def _sub_op(self) -> None:
+        # X = X - mem[address]
+        ##6: PC <- PC + 1; fetch; goto 7
+        self.firmware[6] = 0b000000111_000_00_110101_001000_001_001
+        ##7: MAR <- MBR; read; goto 8
+        self.firmware[7] = 0b000001000_000_00_010100_100000_010_010
+        ##8: H <- MDR; goto 9
+        self.firmware[8] = 0b000001001_000_00_010100_000001_000_000
+        ##9: X <- X - H; goto 0
+        self.firmware[9] = 0b000000000_000_00_111111_000100_000_011
+
+    def _mov_op(self) -> None:
+        # mem[address] = X
+        ##10: PC <- PC + 1; fetch; GOTO 11
+        self.firmware[10] = 0b000001011_000_00_110101_001000_001_001
+        ##11: MAR <- MBR; GOTO 12
+        self.firmware[11] = 0b000001100_000_00_010100_100000_000_010
+        ##12: MDR <- X; write; GOTO 0
+        self.firmware[12] = 0b000000000_000_00_010100_010000_100_011
+
+    def _goto_op(self) -> None:
+        # goto address
+        ##13: PC <- PC + 1; fetch; GOTO 14
+        self.firmware[13] = 0b000001110_000_00_110101_001000_001_001
+        ##14: PC <- MBR; fetch; GOTO MBR
+        self.firmware[14] = 0b000000000_100_00_010100_001000_001_010
+
+    def _jz_op(self) -> None:
+        # if X = 0 then goto address
+        ## 15: X <- X; IF ALU = 0 GOTO 272(100010000) ELSE GOTO 16(000010000)
+        self.firmware[15] = 0b000010000_001_00_010100_000100_000_011
+        ## 16: PC <- PC + 1; GOTO 0
+        self.firmware[16] = 0b000000000_000_00_110101_001000_000_001
+        ## 272: GOTO 13
+        self.firmware[17] = 0b000001101_000_00_000000_000000_000_000
+
     def _control(self) -> None:
         # C => MAR, MDR, PC, X, Y, H
         # B => 000 = MDR, 001 = PC, 010 = MBR, 011 = x, 100 = Y
-        # main: PC <- PC + 1; MBR <- read_byte(PC); GOTO MBR
-        self.add_instruction(0b000000000_100_00_110101_001000_001_001)
+        self._main_op()
 
-        self._last_inst_idx = 2
-        # X = X + mem[address]
-        ##2: PC <- PC + 1; MBR <- read_byte(PC); GOTO 3
-        self.add_instruction(0b000000011_000_00_110101_001000_001_001)
-        ##3: MAR <- MBR; read_word; GOTO 4
-        self.add_instruction(0b000000100_000_00_010100_100000_010_010)
-        ##4: H <- MDR; GOTO 5
-        self.add_instruction(0b000000101_000_00_010100_000001_000_000)
-        ##5: X <- H + X; GOTO 0
-        self.add_instruction(0b000000000_000_00_111100_000100_000_011)
+        self._add_op()  # X = X + mem[address]
+        self._sub_op()  # X = X - mem[address]
+        self._mov_op()  # mem[address] = X
+        self._goto_op()  # goto address
+        self._jz_op()  # if X = 0 then goto address
 
-        # X = X - mem[address]
-        ##6: PC <- PC + 1; fetch; goto 7
-        self.add_instruction(0b000000111_000_00_110101_001000_001_001)
-        ##7: MAR <- MBR; read; goto 8
-        self.add_instruction(0b000001000_000_00_010100_100000_010_010)
-        ##8: H <- MDR; goto 9
-        self.add_instruction(0b000001001_000_00_010100_000001_000_000)
-        ##9: X <- X - H; goto 0
-        self.add_instruction(0b000000000_000_00_111111_000100_000_011)
-
-        # mem[address] = X
-        ##10: PC <- PC + 1; fetch; GOTO 11
-        self.add_instruction(0b000001011_000_00_110101_001000_001_001)
-        ##11: MAR <- MBR; GOTO 12
-        self.add_instruction(0b000001100_000_00_010100_100000_000_010)
-        ##12: MDR <- X; write; GOTO 0
-        self.add_instruction(0b000000000_000_00_010100_010000_100_011)
-
-        # goto address
-        ##13: PC <- PC + 1; fetch; GOTO 14
-        self.add_instruction(0b000001110_000_00_110101_001000_001_001)
-        ##14: PC <- MBR; fetch; GOTO MBR
-        self.add_instruction(0b000000000_100_00_010100_001000_001_010)
-
-        # if X = 0 then goto address
-        ## 15: X <- X; IF ALU = 0 GOTO 272(100010000) ELSE GOTO 16(000010000)
-        self.add_instruction(0b000010000_001_00_010100_000100_000_011)
-        ## 16: PC <- PC + 1; GOTO 0
-        self.add_instruction(0b000000000_000_00_110101_001000_000_001)
-        ## 272: GOTO 13
-        self.add_instruction(0b000001101_000_00_000000_000000_000_000, 272)
-
-        # halt:
         self.halt()
